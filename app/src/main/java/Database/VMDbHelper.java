722d6b0c5af1da07e703bc49e6f4dcff5f8170d8
josh <joshogun>
2019-02-20 23:53:56 +0000

    This is the beginning of the database creation.

---
+++ app/src/main/java/Database/VMDbHelper.java
@@ -0,0 +1,29 @@
+package Database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class VMDbHelper extends SQLiteOpenHelper {
+    // If you change the database schema, you must increment the database version.
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "VM.db";
+
+    public VMDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+    public void onCreate(SQLiteDatabase db) {
+       // db.execSQL(SQL_CREATE_TASKS);
+      //  db.execSQL(SQL_CREATE_EVENTS);
+    }
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // This database is only a cache for online data, so its upgrade policy is
+        // to simply to discard the data and start over
+       //db.execSQL(SQL_DELETE_TASKS);
+        //db.execSQL(SQL_DELETE_EVENTS);
+        onCreate(db);
+    }
+    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        onUpgrade(db, oldVersion, newVersion);
+    }
+}
