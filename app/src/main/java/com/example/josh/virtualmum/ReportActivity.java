611657f2c67b2065d914e02b488ee1f005bd0e14
sd958 <47761646+sd958@users.noreply.github.com>
2019-03-28 14:53:39 +0000

    Start of report graph

---
+++ app/src/main/java/com/example/josh/virtualmum/ReportActivity.java
@@ -0,0 +1,29 @@
+package com.example.josh.virtualmum;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.jjoe64.graphview.*;
+import com.jjoe64.graphview.series.BarGraphSeries;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import Database.Progress.Progress;
+import Database.Task.Task;
+import Database.VMDbHelper;
+
+public class ReportActivity extends AppCompatActivity {
+
+ 
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+    
+    }
+
+
+}
