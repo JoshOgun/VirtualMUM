5dd3c1bf733a4da0a5bc955c69a94df1bed95c7e
sd958 <47761646+sd958@users.noreply.github.com>
2019-03-28 15:10:14 +0000

    Start of 2 graphs

--- app/src/main/java/com/example/josh/virtualmum/ReportActivity.java
+++ app/src/main/java/com/example/josh/virtualmum/ReportActivity.java
@@ -13,8 +13,8 @@ import com.jjoe64.graphview.series.LineGraphSeries;
 import java.util.ArrayList;
 import java.util.List;
 
-import Database.Progress.Progress;
 import Database.Task.Task;
+import Database.Timetable.Timetable;
 import Database.VMDbHelper;
 
 public class ReportActivity extends AppCompatActivity {
@@ -22,7 +22,29 @@ public class ReportActivity extends AppCompatActivity {
  
     @Override
     protected void onCreate(Bundle savedInstanceState){
-    
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_progress_graph);
+        
+        long taskID;
+        
+        //array for dates, amount of work done , expected amount of work done - needs to be part of 2d array
+     
+     //maybe need to get context here instead of (GraphView)
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        BarGraphSeries<DataPoint> WorkDone = new BarGraphSeries<>();
+     
+        BarGraphSeries<DataPoint> ExpectedWork = new BarGraphSeries<>();
+     
+     
+     //customise
+        WorkDone.setSpacing(50);
+        WorkDone.setDrawValuesOnTop(true);
+        WorkDone.setValuesOnTopColor(Color.BLUE);
+     
+        ExpectedWork.setSpacing(50);
+        ExpectedWork.setDrawValuesOnTop(true);
+        ExpectedWork.setValuesOnTopColor(Color.RED);
+     
     }
 
 
